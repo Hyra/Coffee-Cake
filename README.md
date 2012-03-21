212598cf6f3fc8b751064f254091edddaf8d2822
Hyra <stef@mindthecode.com>
2012-03-21 11:49:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+## Coffee Helper
+
+This little Helper converts your .coffee files into .js without relying on Node.js or client-side parsing.
+Everything is compiled on the server, cached, and served as regular js through PHP.
+
+## Installation
+
+### Get the files in place
+
+Clone from github: in your plugin directory type `git clone https://github.com/Hyra/Coffee-Cake.git Coffee`
+You should end up having a folder `app/Plugin/Coffee`
+
+### Create the coffee folder
+
+- Create a folder called `coffee` in `app/webroot/`
+- Optionally apply `chmod 777` to your `js` folder. (The Coffee Helper will place all compiled js files in your js-directory.)
+
+## Usage
+Where you want to use Coffee files, add the helper. Usually this will be your `AppController`.
+
+	public $helpers = array('Coffee.Coffee');
+
+Next, simply add the Coffee files to your views:
+
+	echo $this->Coffee->script('yourfile');
+	
+or
+	
+	echo $this->Coffee->script(array(
+			'somedir/somefile',
+			'awesome_script',
+		)
+	);
+
+That's it.
+
---
+++ View/Helper/CoffeeHelper.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Coffee to JS Converter Plugin
+ *
+ * This plugin compiles your .coffee files to regular JS withouth relying on
+ * client- or serverside JavaScript such as node
+ *
+ * Relies on the coffeescript class in Vendor
+ *
+ * PHP versions 5.3+ as it uses namespaces and anonymous functions
+ *
+ * Mindthecode: http://www.mindthecode.com
+ * Copyright 2011, Stef van den Ham
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author        Stef van den Ham
+ * @copyright     Copyright 2011, Mindthecode (http://www.mindthecode.com)
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+App::uses('Folder', 'Utility');
+App::uses('File', 'Utility');
+App::uses('Component', 'Controller');
+
+require_once(APP . 'Plugin/Coffee/Vendor/coffeescript/coffeescript.php');
+
+class CoffeeHelper extends AppHelper {
+	
+	public $helpers = array('Html');
+	
+	public function __construct(View $View, $options = array()) {
+		parent::__construct($View, $options);
+		$this->coffeeFolder = new Folder(APP . 'webroot' . DS . 'coffee');
+		$this->jsFolder = new Folder(APP . 'webroot' . DS . 'js');
+	}
+	
+	public function script($file) {
+		if(is_array($file)) {
+			foreach($file as $candidate) {
+				$source = $this->coffeeFolder->path . DS . $candidate . '.coffee';
+				if(file_exists($source)) {
+					$target = str_replace('.coffee', '.js', str_replace($this->coffeeFolder->path, $this->jsFolder->path, $source));
+					$this->auto_compile_coffee($source, $target);
+				}
+			}
+		} else {
+			$source = $this->coffeeFolder->path . DS . $file . '.coffee';
+			if(file_exists($source)) {
+				$target = str_replace('.coffee', '.js', str_replace($this->coffeeFolder->path, $this->jsFolder->path, $source));
+				$this->auto_compile_coffee($source, $target);
+			}
+		}
+		echo $this->Html->script($file);
+	}
+
+	public function auto_compile_coffee($coffee_fname, $js_fname) {
+		if(filemtime($js_fname) < filemtime($coffee_fname)) {
+			$coffeeScript = file_get_contents($coffee_fname);
+			if($coffeeScript !== '') {
+				$new_cache = CoffeeScript\compile($coffeeScript);
+				$jsFile = new File($js_fname, TRUE);
+				$jsFile->write($new_cache);
+			}
+		}
+	}
+
+};
